611be56ae0602b2b9d2f8036bafaa6f852a122bb
yn5 <ynsnetmail@gmail.com>
2016-04-13 12:38:01 +0200

    init

---
+++ main.js
@@ -0,0 +1,20 @@
+var allElements = document.getElementsByTagName('*');
+var fontStyles = [];
+var computedStyle;
+var styles;
+
+var extractFontStyles = function () {
+  [].forEach.call(allElements, function (element) {
+    computedStyle = window.getComputedStyle(element);
+    styles = JSON.stringify({
+      family: computedStyle.getPropertyValue('font-family'),
+      size: computedStyle.getPropertyValue('font-size'),
+      weight: computedStyle.getPropertyValue('font-weight')
+    });
+
+    if (fontStyles.indexOf(styles) === -1) {
+      fontStyles.push(styles)
+    }
+  });
+  console.log(fontStyles);
+}()
